fa7d164416492f7ee82fcad9d5b99b57b343558c
Hayoon <gkdbssla97@gmail.com>
2022-07-30 01:29:02 +0900

    .

--- src/main/java/whale/whale_Project/domain/Address.java
+++ src/main/java/whale/whale_Project/domain/Address.java
@@ -1,20 +1,19 @@
 package whale.whale_Project.domain;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 import javax.persistence.Embeddable;
 import javax.persistence.Embedded;
 
 @Embeddable
 @Getter
+@RequiredArgsConstructor
 public class Address {
 
     private String city;
     private String street;
     private String zipcode;
-
-    protected Address() {
-    }
 
     public Address(String city, String street, String zipcode) {
         this.city = city;
--- src/main/java/whale/whale_Project/repository/ResultRepository.java
+++ src/main/java/whale/whale_Project/repository/ResultRepository.java
@@ -1,15 +1,34 @@
 package whale.whale_Project.repository;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import whale.whale_Project.domain.Result;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.List;
 
 @Repository
+@RequiredArgsConstructor
 public class ResultRepository {
 
     @PersistenceContext
     private EntityManager em;
 
+    public void save(Result result) {
+        if (result.getId() == null) {
+            em.persist(result);
+        } else {
+            em.merge(result);
+        }
+    }
 
+    public Result findOne(Long id) {
+        return em.find(Result.class, id);
+    }
+
+    public List<Result> findAll() {
+        return em.createQuery("select r from Result r", Result.class)
+                .getResultList();
+    }
 }
